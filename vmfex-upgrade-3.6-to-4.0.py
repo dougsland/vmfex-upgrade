5f8a6a38e44960bd12cc13c45a9aaa5a46e0f458
Ala Hino <ala.heno@gmail.com>
2017-04-13 14:04:57 +0300

    Update script and README

--- vmfex-upgrade-3.6-to-4.0.py
+++ vmfex-upgrade-3.6-to-4.0.py
@@ -17,68 +17,89 @@
 # limitations under the License.
 #
 
+import argparse
 import ast
 import getpass
 import logging
+import sys
 
 from ovirtsdk.api import API
 from ovirtsdk.xml import params
 
 logging.basicConfig(level=logging.DEBUG, filename='vmfex-upgrade.log')
 
-api_url = raw_input("Please enter RHV Manager API url: ")
-username = raw_input("Please enter RHV Manager admin name: ")
-password = getpass.getpass(prompt='Please enter RHV Manager admin password: ')
-vm_name = raw_input("Please enter VM name: ")
-cluster = raw_input("Please enter destination cluster name: ")
+def get_vmfex(vm):
+    if vm.custom_properties:
+        for custom_property in vm.custom_properties.custom_property:
+            if custom_property.name == "vmfex":
+                return custom_property.value
+    return None
 
-api = API(url=api_url, username=username, password=password)
+def get_profiles(vmfex):
+    vmfex_dict = ast.literal_eval(vmfex)
+    profile_name_to_id = {}
+    for profile in api.vnicprofiles.list():
+        if profile.name in vmfex_dict.values():
+            profile_name_to_id[profile.name] = profile.id
 
-vm = api.vms.get(vm_name)
+    return profile_name_to_id
 
-if vm.status.state != 'down':
-    logging.info("The VM is not down. Exiting ...")
-    exit()
-
-vmfex = None
-if vm.custom_properties:
-    for custom_property in vm.custom_properties.custom_property:
-        if custom_property.name == "vmfex":
-            vmfex = custom_property.value
-
-if vmfex == None:
-    logging.info("No vmfex property found. Exiting ...")
-    exit()
-logging.info("vmfex value: %s", vmfex)
-
-vmfex_dict = ast.literal_eval(vmfex)
-
-profile_name_to_id = {}
-for profile in api.vnicprofiles.list():
-    if profile.name in vmfex_dict.values():
-        profile_name_to_id[profile.name] = profile.id
-
-for (profile_name, profile_id) in profile_name_to_id.items():
-    try:
-        logging.info("Adding profile name: %s, id: %s", profile_name, profile_id)
-        vm.nics.add(
-            params.NIC(
-                name=profile_name,
-                description='',
-                vnic_profile=params.VnicProfile(
-                    id=profile_id,
+def add_profiles(vm, vmfex):
+    for (profile_name, profile_id) in get_profiles(vmfex).items():
+        try:
+            logging.info("Adding profile name: %s, id: %s", profile_name, profile_id)
+            vm.nics.add(
+                params.NIC(
+                    name=profile_name,
+                    description='',
+                    vnic_profile=params.VnicProfile(
+                        id=profile_id,
+                    )
                 )
             )
-        )
+        except Exception as e:
+            # probably adding an existing vnic. Ignore this exception
+            logging.exception(e)
+
+def move_vm_to_cluster(vm, cluster):
+    logging.info("Moving the VM to cluster: %s", cluster)
+    vm.cluster = params.Cluster(name=cluster)
+    try:
+        vm.update()
     except Exception as e:
-        # probably adding an existing vnic. Ignore this exception
+        # getting exception that VM cannot be updated if some of the specified
+        # custom properties are not configured by the system. Ignoring this exception.
         logging.exception(e)
 
-logging.info("Moving the VM to cluster: %s", cluster)
-vm.cluster = params.Cluster(name=cluster)
-try:
-    vm.update()
-except Exception as e:
-    # getting exception that VM cannot be updated if some of the specified
-    # custom properties are not configured by the system. Ignoring this exception.
-    logging.exception(e)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="username")
+    parser.add_argument("-url", "--url", help="API URL")
+    parser.add_argument("-vm", "--vmname", help="VM name")
+    parser.add_argument("-c", "--cluster", help="Cluster name")
+    if len(sys.argv) != 9:
+        parser.print_help()
+        exit()
+
+    args = parser.parse_args()
+    password = getpass.getpass(prompt='Please enter RHV Manager admin password: ')
+
+    api = API(url=args.url, username=args.username, password=password)
+
+    vm = api.vms.get(args.vmname)
+    if vm.status.state != 'down':
+        print("The VM is not down. Exiting ...")
+        logging.info("The VM is not down. Exiting ...")
+        exit()
+
+    vmfex = get_vmfex(vm)
+    if vmfex == None:
+        print("No vmfex property found. Exiting ...")
+        logging.info("No vmfex property found. Exiting ...")
+        exit()
+
+    logging.info("vmfex value: %s", vmfex)
+
+    add_profiles(vm, vmfex)
+
+    move_vm_to_cluster(vm, args.cluster)
